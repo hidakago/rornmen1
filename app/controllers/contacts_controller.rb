94e5076b7f2a5d29f429e7a65966911a0ec1dda2
hidakago <hidakago@gmail.com>
2019-07-25 11:10:44 +0900

    Rails入門演習1

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "お問い合わせを投稿しました！"
+    else
+      # 入力フォームを再描画します。
+      render 'new'
+    end
+  end
+end
+
+private
+
+def contact_params
+  params.require(:contact).permit(:name,:email, :content)
+end
